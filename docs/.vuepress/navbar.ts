981a3a3f5a3660eaf92539875e1369a65747867d
yzqdev <yzqdev@outlook.com>
2022-05-06 23:39:04 +0800

    update vuepress

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,6 +1,6 @@
-import { defineNavbarConfig } from "vuepress-theme-hope";
+import { navbar } from "vuepress-theme-hope";
 
-export default defineNavbarConfig([
+export default navbar([
   // { text: "文章", icon: "home", link: "/article/" },
 
   { text: "bash教程", icon: "shell", link: "/bash-tutorial/" },
