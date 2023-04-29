4aae665cc17706a818ac8366ac420dc878b3d71f
yzqdev <yzqdev@outlook.com>
2023-04-29 09:22:31 +0800

    update width, 2023-04-29 09:22:30

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,48 +1,51 @@
 import theme from "./themeConfig";
-import { defineUserConfig } from "vuepress";
+import {defineUserConfig} from "vuepress";
 import {searchProPlugin} from "vuepress-plugin-search-pro";
+import {autoCatalogPlugin} from 'vuepress-plugin-auto-catalog'
 export default defineUserConfig({
-  base: '/wangdoc/',
+    base: '/wangdoc/',
 
-  dest: './dist',
+    dest: './dist',
 
-  head: [
-    [
-      'link',
-      {
-        rel: 'icon',
-        type: 'image/png',
+    head: [
+        [
+            'link',
+            {
+                rel: 'icon',
+                type: 'image/png',
 
-        href: `/wangdoc/images/wangdoc.png`,
-      },
+                href: `/wangdoc/images/wangdoc.png`,
+            },
+        ],
+        [
+            'link',
+            {
+                rel: 'stylesheet',
+                href: '//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css',
+            },
+        ],
+        [
+            'link',
+            //我的iconfont库
+            {
+                rel: 'stylesheet',
+                href: '//at.alicdn.com/t/font_3267094_j92iwdcfcxp.css',
+            },
+        ],
     ],
-    [
-      'link',
-      {
-        rel: 'stylesheet',
-        href: '//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css',
-      },
-    ],
-    [
-      'link',
-      //我的iconfont库
-      {
-        rel: 'stylesheet',
-        href: '//at.alicdn.com/t/font_3267094_j92iwdcfcxp.css',
-      },
-    ],
-  ],
 
-  locales: {
-    '/': {
-      lang: 'zh-CN',
-      title: 'wangdoc',
-      description: 'wangdoc',
+    locales: {
+        '/': {
+            lang: 'zh-CN',
+            title: 'wangdoc',
+            description: 'wangdoc',
+        },
     },
-  },
 
-  theme,
-  plugins: [searchProPlugin({
-    indexContent:true
-  })],
+    theme,
+    plugins: [
+        autoCatalogPlugin(),
+        searchProPlugin({
+        indexContent: true
+    })],
 })
--- docs/.vuepress/themeConfig.ts
+++ docs/.vuepress/themeConfig.ts
@@ -1,60 +1,60 @@
-import { hopeTheme } from "vuepress-theme-hope";
+import {hopeTheme} from "vuepress-theme-hope";
 import navbar from "./navbar";
 import sidebar from "./sidebar";
 
 export default hopeTheme({
-  hostname: "https://yzqbooks.github.io/wangdoc",
+    hostname: "https://yzqbooks.github.io/wangdoc",
 
-  author: {
-    name: "yzqdev",
-    url: "http://www.yzqdev.top",
-  },
-
-  iconPrefix: "iconfont icon-",
-
-  logo: "/images/wangdoc.png",
-
-  repo: "https://github.com/yzqbooks/wangdoc",
-
-  docsDir: "docs",
-
-  // navbar
-  navbar: navbar,
-
-  // sidebar
-  sidebar: sidebar,
-
-  footer: `powered by <a href='https://vuepress-theme-hope.github.io/v2/'>vuepress-theme-home</a>`,
-
-  displayFooter: true,
-
-  pageInfo: ["Author", "Original", "Date", "Category", "Tag", "ReadingTime"],
- 
-  // encrypt: {
-  //   config: {
-  //     "/guide/encrypt.html": ["1234"],
-  //   },
-  // },
-
-  plugins: {
-    blog: false,
-
-    // 你也可以使用 Waline
-    // comment: {
-    //   type: "giscus",
-    //   repo: "yzqdev/blog-issues",
-    //   repoId: "MDEwOlJlcG9zaXRvcnkzODI2NTY1MTY=",
-    //   category: "General",
-    //   categoryId: "DIC_kwDOFs7gBM4COKgn",
-    // },
-prismjs:{
-  dark:'tomorrow',
-  light:'tomorrow'
-},
-    mdEnhance: {
-      presentation: {
-        plugins: ["highlight", "math", "search", "notes", "zoom"],
-      },
+    author: {
+        name: "yzqdev",
+        url: "http://www.yzqdev.top",
     },
-  },
-});
+    pcBreakPoint: 1600,
+    iconPrefix: "iconfont icon-",
+
+    logo: "/images/wangdoc.png",
+
+    repo: "https://github.com/yzqbooks/wangdoc",
+
+    docsDir: "docs",
+
+    // navbar
+    navbar: navbar,
+
+    // sidebar
+    sidebar: sidebar,
+
+    footer: `powered by <a href='https://vuepress-theme-hope.github.io/v2/'>vuepress-theme-home</a>`,
+
+    displayFooter: true,
+
+    pageInfo: ["Author", "Original", "Date", "Category", "Tag", "ReadingTime"],
+
+    // encrypt: {
+    //   config: {
+    //     "/guide/encrypt.html": ["1234"],
+    //   },
+    // },
+
+    plugins: {
+        blog: false,
+
+        // 你也可以使用 Waline
+        // comment: {
+        //   type: "giscus",
+        //   repo: "yzqdev/blog-issues",
+        //   repoId: "MDEwOlJlcG9zaXRvcnkzODI2NTY1MTY=",
+        //   category: "General",
+        //   categoryId: "DIC_kwDOFs7gBM4COKgn",
+        // },
+        prismjs: {
+            dark: 'tomorrow',
+            light: 'tomorrow'
+        },
+        mdEnhance: {
+            presentation: {
+                plugins: ["highlight", "math", "search", "notes", "zoom"],
+            },
+        },
+    },
+}, {custom: true});
